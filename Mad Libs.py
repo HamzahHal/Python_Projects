4ee85869e552298f5cf2d6d4f8364b552c65c0d6
HamzahHal <hamzah.halim@hotmail.com>
2021-07-29 19:10:48 +0100

    commit

---
+++ Mad Libs.py
@@ -0,0 +1,8 @@
+
+colour = input("Enter your favourite colour: ")
+sound = input("Enter the most annoying sound: ")
+smell = input("Enter the worst smell: ")
+
+print(colour + " is the colour of your eyes,")
+print(smell + " is the scent of your loved on,")
+print(sound + " is the sound of your worst nightmate..")
