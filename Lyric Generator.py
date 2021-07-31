5fdedab69d1d585b7acfee3fee2c18b0198c5312
HamzahHal <hamzah.halim@hotmail.com>
2021-07-31 23:44:11 +0100

    commit

--- Lyric Generator.py
+++ Lyric Generator.py
@@ -1,34 +1,19 @@
 
 Songs = {
 
-    '1': 'Baby by Bieber',
-    '2': 'Hotline Bling by Drake',
-    '3': 'Flawless by Beyonce',
-    '4': 'Fall by Eminem'
+    'Baby by Bieber': '1',
 }
 
 Choice = ""
 Selected = False
 print(" 1: 'Baby by Bieber',")
-print(" 2: 'Hotline Bling by Drake',")
-print(" 3: 'Flawless by Beyonce',")
-print(" 4: 'Fall by Eminem'")
 
-while Choice != Songs.items() and not(Selected):
+while Choice != Songs['Baby by Bieber'] and not(Selected):
     # if Selected == False:
     Choice = input('Enter Song: ')
 
-if Choice == Songs.keys([1]):
-    print("Baby by Bieber")
-    Selected = True#
-elif Choice == Songs['Hotline Bling by Drake']:
-    print("Hotline Bling")
-    Selected = True
-elif Choice == Songs['Flawless by Beyonce']:
-    print("Flawless tings")
-    Selected = True
-elif Choice == Songs['Fall by Eminem']:
-    print("Fall bitches with stitches")
+if Choice == Songs['Baby by Bieber']:
+    print("Baby, Baby, Baby, Ooooo ")
     Selected = True
 
 
