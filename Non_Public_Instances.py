26033692995bdbab282bf9895030859904f55b1a
HamzahHal <hamzah.halim@hotmail.com>
2021-08-03 06:18:14 +0100

    commit

--- Non_Public_Instances.py
+++ Non_Public_Instances.py
@@ -2,13 +2,15 @@ from External_Instance import External
 class abc():
     __a = input("Enter Value: ")
     def disp(self):
-        ahf = 20
+        ahf = input('Enter External Object Value: ')
         print(self.__a)
         print(ahf)
+        # Preset variable from an external class
         print(External.Test)
 
+# this allows the values to be shown from within the disp function as well as the values which are imported from the External Class & Method
 obj=abc()
-# obj.disp()
+obj.disp()
 
 
 
