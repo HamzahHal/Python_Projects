bcf2907d457a3129a42a1d53d79f1a450f2cb3d0
HamzahHal <hamzah.halim@hotmail.com>
2021-08-03 06:14:03 +0100

    commit

---
+++ External_Instance.py
@@ -0,0 +1,4 @@
+class External():
+    Test = 10
+    def qwer(self, ahf):
+        self.ahf
--- Non_Public_Instances.py
+++ Non_Public_Instances.py
@@ -1,12 +1,14 @@
-
+from External_Instance import External
 class abc():
     __a = input("Enter Value: ")
     def disp(self):
+        ahf = 20
         print(self.__a)
-
-
+        print(ahf)
+        print(External.Test)
 
 obj=abc()
- obj.disp()
+# obj.disp()
+
 
 
